38b975273a5660eae59b91d2df38bc277c6730ac
LosBergmannos <121161285+LosBergmannos@users.noreply.github.com>
2022-12-21 10:32:33 +0100

    Add files via upload
    idk

---
+++ c++ forcyc.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 0;
+    for (int i =0; i <1; i++) {
+
+        cout <<"      * " << "\n";
+        cout <<"     ***" << "\n";
+        cout <<"    *****" << "\n";
+        cout <<"   *******" << "\n";
+        cout <<"  *********" << "\n";
+
+}
+}
